cb2cd57f3b3e154a377dfdc3bf5fba0a76487b6e
romanchervyakov <roman.cherviakov@innovecs.com>
2020-09-05 20:40:15 +0300

    test

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,5 +1,5 @@
 package graph
-
+//go:generate go run github.com/99designs/gqlgen
 import "github.com/romancherviakov/gqlgen-todos/graph/model"
 
 // This file will not be regenerated automatically.
